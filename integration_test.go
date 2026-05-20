24315f7eb04858b93623f44fb4c8f8c14c351cd3
cvdtang <47664451+cvdtang@users.noreply.github.com>
2026-05-20 19:34:10 +0200

    test(integration): rancher/k3s v1.36.1 (#31)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- integration_test.go
+++ integration_test.go
@@ -67,7 +67,7 @@ const (
 
 	tokenReferencePodImage = "busybox"
 	// renovate: datasource=docker depName=rancher/k3s
-	k3sImage = "rancher/k3s:v1.36.0-k3s1"
+	k3sImage = "rancher/k3s:v1.36.1-k3s1"
 	// renovate: datasource=docker depName=hashicorp/vault
 	vaultVersion = "2.0.1"
 	// renovate: datasource=docker depName=openbao/openbao
